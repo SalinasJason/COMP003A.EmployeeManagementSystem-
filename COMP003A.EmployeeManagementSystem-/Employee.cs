085d2e54cabd60346e5952c0b3c09e296dc6d2e2
SalinasJason <jasonsalinas770@gmail.com>
2025-02-13 13:19:47 -0800

    Added public properties and a constructor

--- COMP003A.EmployeeManagementSystem-/Employee.cs
+++ COMP003A.EmployeeManagementSystem-/Employee.cs
@@ -13,5 +13,42 @@ namespace COMP003A.EmployeeManagementSystem_
         private string _middleName;
         private string _lastName;
         private double _salary;
+
+        public string EmployeeId
+        {
+            get { return _employeeId; }
+            set { _employeeId = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return _middleName; } 
+            set { _middleName = value; }
+        }
+
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public Employee(string inputFirstName, string inputMiddleName, string inputLastName)
+        {
+            _firstName = inputFirstName;
+            _middleName = inputMiddleName;
+            _lastName = inputLastName;
+        }
     }
 }
