44fb4b38b8523270ed4d37b6713653365f76457d
SalinasJason <jasonsalinas770@gmail.com>
2025-02-13 13:10:33 -0800

    Created a class named employee and added private fields

---
+++ COMP003A.EmployeeManagementSystem-/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    internal class Employee
+    {
+        private string _employeeId;
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        private double _salary;
+    }
+}
