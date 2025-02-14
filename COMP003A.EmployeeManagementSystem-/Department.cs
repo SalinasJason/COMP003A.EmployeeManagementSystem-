1ebc95dd9880115dc0c974237383b9d11ca32ea0
SalinasJason <jasonsalinas770@gmail.com>
2025-02-13 17:44:13 -0800

    Added abstract class named Department

---
+++ COMP003A.EmployeeManagementSystem-/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    internal abstract class Department
+    {
+        public string DepartmentName
+        {
+            get; set;
+        }
+
+        public abstract string GetDepartmentDetails();
+
+        public void DisplayDepartmentInfo()
+        {
+            Console.WriteLine($"{DepartmentName}");
+        }
+    }
+}
