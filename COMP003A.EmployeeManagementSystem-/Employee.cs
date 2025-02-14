9429cf7e9c22a3ebc88a14ccb65449eb02b078ba
SalinasJason <jasonsalinas770@gmail.com>
2025-02-14 14:26:55 -0800

    Added set accessor validation and comments

--- COMP003A.EmployeeManagementSystem-/Employee.cs
+++ COMP003A.EmployeeManagementSystem-/Employee.cs
@@ -9,56 +9,102 @@ namespace COMP003A.EmployeeManagementSystem_
 {
     internal class Employee
     {
+
+        // Fields
         private string _employeeId;
         private string _firstName;
         private string _middleName;
         private string _lastName;
         private double _salary;
 
+        // Properties
         public string EmployeeId
         {
             get { return _employeeId; }
-            set { _employeeId = value; }
+        
         }
 
         public string FirstName
         {
             get { return _firstName; }
-            set { _firstName = value; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw ArgumentException();
+                }
+                _firstName = value;
+            }
         }
 
         public string LastName
         {
             get { return _lastName; }
-            set { _lastName = value; }
+            set {
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                _lastName = value;
+            }
         }
 
         public string MiddleName
         {
             get { return _middleName; } 
-            set { _middleName = value; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException();
+                }
+                _middleName = value;
+            }
         }
 
         public double Salary
         {
             get { return _salary; }
-            set { _salary = value; }
+            set 
+            { 
+                 if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                _salary = value; 
+            }
         }
 
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="inputEmployeeId"></param>
+        /// <param name="inputFirstName"></param>
+        /// <param name="inputMiddleName"></param>
+        /// <param name="inputLastName"></param>
+        /// <param name="inputSalary"></param>
         public Employee(string inputEmployeeId, string inputFirstName, string inputMiddleName, string inputLastName, double inputSalary)
         {
-            EmployeeId = inputEmployeeId;
+            _employeeId = inputEmployeeId;
             FirstName = inputFirstName;
             MiddleName = inputMiddleName;
             LastName = inputLastName;
             Salary = inputSalary;
         }
 
+        /// <summary>
+        /// Prints Full Name
+        /// </summary>
         public void PrintFullName()
         {
             Console.WriteLine($"{FirstName} {MiddleName} {LastName}");
         }
 
+        /// <summary>
+        /// Displays Employee Information
+        /// </summary>
         public void DisplayEmployeeInfo()
         {
             Console.WriteLine($"Employee ID: {EmployeeId}");
