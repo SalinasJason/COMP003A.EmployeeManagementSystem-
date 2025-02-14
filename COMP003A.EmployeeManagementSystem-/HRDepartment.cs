e03f5f00e571d6256a8e80992d894198c42f5302
SalinasJason <jasonsalinas770@gmail.com>
2025-02-13 18:46:38 -0800

    Created two derived classes HRDepartment and ITDepartment

---
+++ COMP003A.EmployeeManagementSystem-/HRDepartment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    internal class HRDepartment : Department
+    {
+
+        public HRDepartment(string departmentName)
+        {
+            DepartmentName = departmentName;
+        }
+ 
+        public override string GetDepartmentDetails()
+        {
+            return "Handles employee relations and recruitment.";
+        }
+    }
+}
---
+++ COMP003A.EmployeeManagementSystem-/ITDepartment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    internal class ITDepartment : Department
+    {
+        public ITDepartment(string departmentName)
+        {
+            DepartmentName = departmentName;
+        }
+
+        public override string GetDepartmentDetails()
+        {
+            return "Manages technical resources and infrastructure.";
+        }
+    }
+}
